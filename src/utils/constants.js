734b2b6948a391c1f83df4d069dd715be548e3d8
Dharan <dharan.v2006@gmail.com>
2025-12-09 22:19:15 +0530

    Update API URLs to use environment variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
-export const API_BASE_URL = 'http://localhost:5000';
-export const SOCKET_URL = 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+
+export { API_BASE_URL, SOCKET_URL };
 
 export const ROUTES = {
   HOME: '/',
